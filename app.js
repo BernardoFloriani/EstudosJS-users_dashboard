134b8e0bef9beca23562017d612d79f1449ce405
BernardoFloriani <bernardofloriani12@gmail.com>
2026-08-07 14:43:27 -0300

    app.js -> add user list to the HTML

--- app.js
+++ app.js
@@ -8,10 +8,29 @@ async function fetchUsers(url) {
         return data
 }
 
+function renderUsers(array) {
+    const userList = document.getElementById('user-list')
+
+    array.forEach(element => {
+        const {name, email, username, address: {city}} = element
+
+        const card = document.createElement('div')
+        card.className = 'card'
+        card.innerHTML = `
+            <h2>${name}</h2>
+            <p>${username}</p>
+            <p>${email}</p>
+            <p>${city}</p>
+        `
+        userList.appendChild(card)
+    });
+}
+
 async function main() {
     try {
         const users = await fetchUsers(usersUrl)
-        console.log(users)
+        console.log('adding users list to the HTML');
+        renderUsers(users)
     } catch (error) {
         console.log(error);
     }
